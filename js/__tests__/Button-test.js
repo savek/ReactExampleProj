1cec33a5167e017fde728820b35e1d2b81c1148e
av.sorokin <A.Sorokin@mils-s.ru>
2020-05-20 22:48:03 +0500

    Добавлен тест компонента Button

--- js/__tests__/Button-test.js
+++ js/__tests__/Button-test.js
@@ -1,15 +1,42 @@
+jest
+  .dontMock('../source/components/Button')
+  .dontMock('classnames')
+;
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
+import Button from "../source/components/Button";
 
-describe('Мы можем отобразить кнопку', () => {
-    it('Кнопка изменяет текст после щелчка', () => {
-        const button = TestUtils.renderIntoDocument(
-            <button onClick={ev => ev.target.innerHTML = 'Bye'}>
-                Hello
-            </button>
-        );
+// const Button = require('../source/components/Button').default;
 
-        expect(ReactDOM.findDOMNode(button).textContent).toEqual('Hello');
-    });
+describe('Render Button components', () => {
+  it('renders <a> vs <button>', () => {
+    const button = TestUtils.renderIntoDocument(
+      <div>
+        <Button>
+          Hello
+        </Button>
+      </div>
+    );
+    expect(ReactDOM.findDOMNode(button).children[0].nodeName).toEqual('BUTTON');
+    
+    const a = TestUtils.renderIntoDocument(
+      <div>
+        <Button href="#">
+          Hello
+        </Button>
+      </div>
+    );
+    expect(ReactDOM.findDOMNode(a).children[0].nodeName).toEqual('A');
+  });
+  
+  it('allows custom CSS classes', () => {
+    const button = TestUtils.renderIntoDocument(
+      <div><Button className="good bye">Hello</Button></div>
+    );
+    const buttonNode = ReactDOM.findDOMNode(button).children[0];
+    expect(buttonNode.getAttribute('class')).toEqual('Button good bye');
+  });
+  
 });
